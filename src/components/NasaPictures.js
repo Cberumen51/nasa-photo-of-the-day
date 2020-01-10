849cc81accb7b7a353e226df433628fee0f5ad6c
Cberumen51 <c.berumen51@gmail.com>
2020-01-09 19:09:29 -0800

    some styling

--- src/components/NasaPictures.js
+++ src/components/NasaPictures.js
@@ -1,9 +1,12 @@
 import React, {useState, useEffect} from "react";
 import axios from "axios";
-// import PictureInfo from "./PictureInfo";
+import {Card, CardImg, CardBody, CardTitle, CardText, CardSubtitle, } from 'reactstrap';
+import {ButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem} from 'reactstrap';
 
-export default function NasaPictures() {
+export default function NasaPictures(props) {
     const [pictures, setPicture] = useState([]);
+    const [dropdownOpen, setOpen] = useState(false);
+    const toggle = () => setOpen(!dropdownOpen)
 
     useEffect(() => {
         axios
@@ -20,11 +23,24 @@ export default function NasaPictures() {
 
     return (
     <div className="picture">
-        <h1>{pictures.title}</h1>
-        <h2>{pictures.copyright}</h2>
-        <h2>{pictures.date}</h2>
-        <img alt="NASA POTD" src={pictures.url}/>
-        <p>{pictures.explanation}</p>
+         {/* <ButtonDropdown isOpen={dropdownOpen} toggle={toggle}>
+                <DropdownToggle caret>
+                    Button
+                </DropdownToggle>
+                <DropdownMenu>
+                    <DropdownItem header>Nav</DropdownItem>
+                    <DropdownItem>Action</DropdownItem>
+                </DropdownMenu>
+            </ButtonDropdown> */}
+        <Card>
+            <CardTitle>{pictures.title}</CardTitle>
+            <CardSubtitle>{pictures.copyright}  {pictures.date}</CardSubtitle>
+            <CardBody>
+            <CardImg alt="NASA POTD" src={pictures.url}/>
+            <CardText>{pictures.explanation}</CardText>
+            </CardBody>
+        </Card>
     </div>
     )
 }
+
